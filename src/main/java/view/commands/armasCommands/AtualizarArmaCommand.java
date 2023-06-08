8029eb272e642d1662ec9803a6f7235863ae656b
Rafael Pichelli <rafael.pichelli4@hotmail.com>
2023-06-08 18:03:32 -0300

    ajuste de atualizaArma

--- src/main/java/view/commands/armasCommands/AtualizarArmaCommand.java
+++ src/main/java/view/commands/armasCommands/AtualizarArmaCommand.java
@@ -1,31 +1,45 @@
 package main.java.view.commands.armasCommands;
 
 import main.java.business.model.Arma;
+import main.java.business.model.enums.Atributo;
 import main.java.view.MainScreenDesktop;
 import main.java.view.commands.Command;
+
+import javax.swing.*;
 
 public class AtualizarArmaCommand implements Command {
 
     private MainScreenDesktop mainScreen;
     private Integer id;
-    private  String nome;
-    private String requisito;
-    private  String atributo;
-    private String dano;
-    private String propriedade;
-    public AtualizarArmaCommand(MainScreenDesktop screen, Integer id, String nome, String requisito, String atributo, String dano, String propriedade) {
+    private  String varName,valor;
+    public AtualizarArmaCommand(MainScreenDesktop screen, Integer id, String varName, String valor) {
         this.mainScreen = screen;
         this.id = id;
-        this.nome = nome;
-        this.requisito = requisito;
-        this.atributo = atributo;
-        this.dano = dano;
-        this.propriedade = propriedade;
+        this.varName = varName;
+        this.valor = valor;
     }
 
     @Override
     public void execute() {
-        //Arma arma = new Arma()
-        //mainScreen.getArmaManager().updateArma(id);
+        Arma arma = new Arma();
+        switch (varName){
+            case  "nome":
+                arma.setNome(valor);
+                break;
+            case "requisito":
+                arma.setRequisito(valor);
+                break;
+            case "atributo":
+                arma.setAtributo(Atributo.valueOf(valor));
+                break;
+            case "dano":
+                arma.setDano(valor);
+                break;
+            case "propriedade":
+                arma.setPropriedades(valor);
+                break;
+        }
+        mainScreen.getArmaManager().updateArma(id,varName,arma);
+        JOptionPane.showMessageDialog(null, "Arma Atualizada com sucesso!");
     }
 }
--- src/main/java/view/screenStates/armasStates/AtualizarArmaScreenState.java
+++ src/main/java/view/screenStates/armasStates/AtualizarArmaScreenState.java
@@ -1,6 +1,7 @@
 package main.java.view.screenStates.armasStates;
 
 import com.itextpdf.text.pdf.PdfAnnotation;
+import main.java.business.model.enums.Atributo;
 import main.java.view.MainScreenDesktop;
 import main.java.view.commands.Command;
 import main.java.view.commands.VoltarTelaCommand;
@@ -14,18 +15,17 @@ import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
 public class AtualizarArmaScreenState extends JFrame implements ScreenState {
-    private JTextField textId;
-    private JTextField textNome;
-    private JTextField textRequisito;
-    private JComboBox<String> dropdownAtributo;
-    private JTextField textDano;
-    private JTextField textPropriedade;
+    private JTextField textId, textValor;
+    private JComboBox<String> dropdownAtributo, dropdownVariavel;
     @Override
     public void handleTela(MainScreenDesktop screen) {
-        setTitle("Criar nova Arma");
+        setTitle("Atualizar Arma");
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         setSize(300, 150);
         setLocationRelativeTo(null);
+
+        String[] valoresVariavel = {"nome","requisito","atributo","dano","propriedades"};
+        dropdownVariavel = new JComboBox<>(valoresVariavel);
 
         String[] valoresAtributos = {"FORÇA", "AGILIDADE", "INTELECTO", "VONTADE", "PERCEPÇÃO", "DEFESA",
                 "SAÚDE", "TXCURA", "TAMANHO", "DESLOCAMENTO", "PODER", "DANO",
@@ -33,34 +33,22 @@ public class AtualizarArmaScreenState extends JFrame implements ScreenState {
         dropdownAtributo = new JComboBox<>(valoresAtributos);
 
         JLabel labelId = new JLabel("Id da arma:");
-        JLabel labelNome= new JLabel("Nome da Arma:");
-        JLabel labelRequisito = new JLabel("Requisito:");
-        JLabel labelAtributo = new JLabel("Atributo:");
-        JLabel labelDano = new JLabel("Dano:");
-        JLabel labelPropriedade = new JLabel("Propriedade:");
+        JLabel labelAtrAtu= new JLabel("Atributo para atualizar:");
+        JLabel labelValor = new JLabel("Valor:");
         textId = new JTextField(5);
-        textNome = new JTextField(20);
-        textRequisito = new JTextField(20);
-        textDano = new JTextField(20);
-        textPropriedade = new JTextField(20);
+        textValor = new JTextField(20);
 
-        JButton CriarBtn = new JButton("Criar");
+        JButton CriarBtn = new JButton("Atualizar");
         JButton VoltarBtn = new JButton("Voltar");
 
         JPanel panel = new JPanel(new FlowLayout());
 
         panel.add(labelId);
         panel.add(textId);
-        panel.add(labelNome);
-        panel.add(textNome);
-        panel.add(labelRequisito);
-        panel.add(textRequisito);
-        panel.add(labelAtributo);
-        panel.add(dropdownAtributo);
-        panel.add(labelDano);
-        panel.add(textDano);
-        panel.add(labelPropriedade);
-        panel.add(textPropriedade);
+        panel.add(labelAtrAtu);
+        panel.add(dropdownVariavel);
+        panel.add(labelValor);
+        panel.add(textValor);
 
         panel.add(CriarBtn);
         panel.add(VoltarBtn);
@@ -71,13 +59,10 @@ public class AtualizarArmaScreenState extends JFrame implements ScreenState {
         CriarBtn.addActionListener(new ActionListener() {
             public void actionPerformed(ActionEvent e) {
                 Integer id = Integer.parseInt(textId.getText());
-                String nome = textNome.getText();
-                String requisito = textRequisito.getText();
-                String atributo = (String) dropdownAtributo.getSelectedItem();
-                String dano = textDano.getText();
-                String propriedade = textPropriedade.getText();
+                String varName = (String) dropdownVariavel.getSelectedItem();
+                String valor = textValor.getText();
 
-                Command atualizarArmaCommand = new AtualizarArmaCommand(screen,id,nome,requisito,atributo,dano,propriedade);
+                Command atualizarArmaCommand = new AtualizarArmaCommand(screen,id,varName,valor);
                 atualizarArmaCommand.execute();
             }
         });
